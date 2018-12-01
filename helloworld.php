19c2ac2b252a8fc9fd33c950f493489d39753343
James <jamesfoot62@gmail.com>
2018-12-01 22:05:49 +0000

    Initial commit

---
+++ helloworld.php
@@ -0,0 +1,23 @@
+<html>
+  <!-- [START csslink] -->
+  <head>
+    <link type="text/css" rel="stylesheet" href="/stylesheets/main.css" />
+  </head>
+  <!-- [END csslink] -->
+  <body>
+    <?php
+    // [START formprocessing]
+    if (array_key_exists('content', $_POST)) {
+        echo "You wrote:<pre>\n";
+        echo htmlspecialchars($_POST['content']);
+        echo "\n</pre>";
+    }
+    // [END formprocessing]
+    ?>
+    <h1>Hello James</h1>
+    <form action="/sign" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Sign Guestbook"></div>
+    </form>
+  </body>
+</html>
